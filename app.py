07b528192973f5fefc2e4a412f0ce957ea75b09f
Steshi <147267439+Steshitaya28@users.noreply.github.com>
2024-06-04 02:34:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,4 +57,5 @@ def home():
     return render_template('index.html', plot_url=plot_url)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
